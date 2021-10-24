a2981cdb0cc7a9b92f7f1f015aad3b179032912f
Yves de Oliveira Ribeiro <yvesdeoliveiraribeiro@gmail.com>
2021-10-24 16:17:55 -0300

    Implementação do formulário.

--- site.js
+++ site.js
@@ -1,7 +1,14 @@
-const menu = document.querySelector('#mobile_menu');
-const menuLinks = document.querySelector('.navbar_menu');
+const menu = document.querySelector("#mobile_menu");
+const menuLinks = document.querySelector(".navbar_menu");
 
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 });
+
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Formulário preenchido com sucesso");
+  }
+}
